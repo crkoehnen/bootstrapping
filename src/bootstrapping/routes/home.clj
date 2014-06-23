e0de794e5a999161f0b5d8ef844ec4fea4cf43cf
crkoehnen <crkoehnen@gmail.com>
2014-06-22 21:43:23 -0500

    adding xs grid

--- src/bootstrapping/routes/home.clj
+++ src/bootstrapping/routes/home.clj
@@ -7,10 +7,10 @@
    [:div.container
     [:h1 "Hello World!"]
     [:div.row
-     [:div.col-sm-6.col-md-6.col-lg-6
+     [:div.col-xs-6.col-sm-6.col-md-6.col-lg-6
       [:p "This is stuff in the first column. "]
       [:p "more first column stuff"]]
-     [:div.col-sm-6.col-md-6.col-lg-6
+     [:div.col-xs-6.col-sm-6.col-md-6.col-lg-6
       [:p "And this is stuff in the second."]]]]))
 
 (defroutes home-routes
