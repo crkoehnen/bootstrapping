5b53fd18e66f7d1d9e8f9c2159f284bed52a54cd
crkoehnen <crkoehnen@gmail.com>
2014-06-22 21:31:32 -0500

    using bootstrap, starting grids

--- src/bootstrapping/routes/home.clj
+++ src/bootstrapping/routes/home.clj
@@ -3,7 +3,15 @@
             [bootstrapping.views.layout :as layout]))
 
 (defn home []
-  (layout/common [:h1 "Hello World!"]))
+  (layout/common
+   [:div.container
+    [:h1 "Hello World!"]
+    [:div.row
+     [:div.col-sm-6.col-md-6.col-lg-6
+      [:p "This is stuff in the first column. "]
+      [:p "more first column stuff"]]
+     [:div.col-sm-6.col-md-6.col-lg-6
+      [:p "And this is stuff in the second."]]]]))
 
 (defroutes home-routes
   (GET "/" [] (home)))
--- src/bootstrapping/views/layout.clj
+++ src/bootstrapping/views/layout.clj
@@ -1,9 +1,13 @@
 (ns bootstrapping.views.layout
-  (:require [hiccup.page :refer [html5 include-css]]))
+  (:require [hiccup.page :refer [html5 include-css include-js]]))
 
 (defn common [& body]
   (html5
     [:head
      [:title "Welcome to bootstrapping"]
-     (include-css "/css/screen.css")]
-    [:body body]))
+     (include-css "/css/screen.css")
+     (include-css "/external/bootstrap/css/bootstrap.min.css")]
+    [:body
+     body
+     (include-js "https://code.jquery.com/jquery.js")
+     (include-js "/external/bootstrap/js/bootstrap.min.js")]))
